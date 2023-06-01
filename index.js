f7db6ba379b4d8d9cf19dde69db46c1c9637929a
arbilgin <arda@mentholprotocol.com>
2023-06-01 22:28:26 +0300

    update index

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 
 // copy-paste your URL provided in your Alchemy.com dashboard
-const ALCHEMY_URL = "https://eth-mainnet.g.alchemy.com/v2/ap4JyKB4C9d3WIoUhqzsMOKlNVviwRmA";
+const ALCHEMY_URL = "";
+const address = "";
 
 axios.post(ALCHEMY_URL, {
   jsonrpc: "2.0",
   id: 1,
   method: "eth_getBalance",
-  params: ["0x808b891a69f2cF52f84228DA61f2F4F5b08297DE", "latest"]
+  params: [address, "latest"]
 }).then((response) => {
   console.log(parseInt(response.data.result));
 });
